0b544861c7e7477138208196f1a70710b824d0b7
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2024-02-16 23:36:18 +0300

    React Full Course

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -6,13 +6,43 @@ const List = () => {
 
 
     const fruits2 = [
-                        {name: 'apple', calories: 95}, 
-                        {name: 'orange', calories: 45}, 
-                        {name: 'banana', calories: 105}, 
-                        {name: 'coconut', calories: 159}, 
-                        {name: 'pineapple', calories: 37}
+                        {
+                            id: 1, 
+                            name: 'apple', 
+                            calories: 95
+                        }, 
+                        {
+                            id: 2, 
+                            name: 'orange', 
+                            calories: 45
+                        }, 
+                        {
+                            id: 3, 
+                            name: 'banana', 
+                            calories: 105
+                        }, 
+                        {
+                            id: 4, 
+                            name: 'coconut', 
+                            calories: 159
+                        }, 
+                        {
+                            id: 5, 
+                            name: 'pineapple',
+                            calories: 37
+                        }
                     ];
-    const listItems = fruits2.map(fruit => <li key={fruit.name}>{fruit.name}</li>)
+    // fruits2.sort((a, b) => a.name.localeCompare(b.name)); //Alphabetical order
+    //fruits2.sort((a, b) => b.name.localeCompare(a.name)); //Reverse Alphabetical order
+
+    //fruits2.sort((a, b) => a.calories - b.calories); // Numeric order
+    //fruits2.sort((a, b) => b.calories - a.calories); // Reverse Numeric order
+
+    const listItems = fruits2.map(fruit => 
+                                        <li key={fruit.id}>
+                                            {fruit.name} : &nbsp;<b>{fruit.calories}</b>
+                                        </li>
+                                );
 
     return(<ol>{listItems}</ol>);
 
