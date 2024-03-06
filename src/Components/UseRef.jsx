8d33d94b6b68530d34c83eeabd2c129db7d21a57
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2024-03-06 15:51:11 +0300

    React Full Course

--- src/Components/UseRef.jsx
+++ src/Components/UseRef.jsx
@@ -6,12 +6,57 @@
 // 2. Handling Focus, Animations, and Transitions
 // 3. Managing Timers and Intervals.
 
-import { useState,  } from "react"
+import { useRef, useEffect  } from "react"
 
 const UseRef = () => {
+  // let[number, setNumber] = useState(0);
+
+  const inputRef1 = useRef(0);
+  const inputRef2 = useRef(0);
+  const inputRef3 = useRef(0);
+
+useEffect(() => {
+  console.log('COMPONENT RENDERED');
+});
+
+  const handleClick1 = () => {
+    inputRef1.current.focus();
+    inputRef1.current.style.backgroundColor = 'blue';
+    inputRef2.current.style.backgroundColor = '';
+    inputRef3.current.style.backgroundColor = '';
+  };
+
+  const handleClick2 = () => {
+    inputRef2.current.focus();
+    inputRef1.current.style.backgroundColor = '';
+    inputRef2.current.style.backgroundColor = 'blue';
+    inputRef3.current.style.backgroundColor = '';
+  };
+
+  const handleClick3 = () => {
+    inputRef3.current.focus();
+    inputRef1.current.style.backgroundColor = '';
+    inputRef2.current.style.backgroundColor = '';
+    inputRef3.current.style.backgroundColor = 'blue';
+  };
+
   return (
-    <div>UseRef</div>
-  )
-}
+    <div>
+      <input ref={inputRef1} /> 
+      <br />
+      <button onClick={handleClick1}>Click Me 1!</button>
+      <br />
+
+      <input ref={inputRef2} /> 
+      <br />
+      <button onClick={handleClick2}>Click Me 2!</button>
+      <br />
+      
+      <input ref={inputRef3} /> 
+      <br />
+      <button onClick={handleClick3}>Click Me 3!</button>
+    </div>
+  );
+};
 
 export default UseRef
